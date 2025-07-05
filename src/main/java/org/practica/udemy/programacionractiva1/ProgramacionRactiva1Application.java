2c105f6e48cee999e799def58da3fa9c7d6229bb
Jorge Miguel Mejia <jorgemiguelfv05@gmail.com>
2025-07-05 18:53:00 -0500

    Project init

---
+++ src/main/java/org/practica/udemy/programacionractiva1/ProgramacionRactiva1Application.java
@@ -0,0 +1,13 @@
+package org.practica.udemy.programacionractiva1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionRactiva1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramacionRactiva1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/practica/udemy/programacionractiva1/ProgramacionRactiva1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practica.udemy.programacionractiva1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionRactiva1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
